64c8ee6347a09f48d96536bfe28d9ebe01b1545a
mjimeneztn <mjimeneztn@gmail.com>
2020-06-14 17:44:17 -0500

    form validation for creating new bill

--- src/Components/AddBills/AddBill.js
+++ src/Components/AddBills/AddBill.js
@@ -1,14 +1,47 @@
- import React, {useContext, useState, useEffect} from 'react';
- import './style.css';
+import React, { useContext, useState } from 'react';
+import './style.css';
 
- const AddBill = () => {
-   const [BillTitle, setBillTitle] = useState('');
-   const [BillCost, setBillCost] = useState('');
+const AddBill = () => {
+    const [BillTitle, setBillTitle] = useState('');
+    const [BillCost, setBillCost] = useState('');
 
-     return (
-         <div className='add-bill-container'>
+    const updateBills = () => {
+        console.log(BillTitle, BillCost)
+    }
 
-         </div>
-          );
- };
+    const billObjectValid = () => {
+        // BillCost is truthy and is a number
+        const costValid = BillCost && Number.parseFloat(BillCost)
+
+        //BillTitle is truthy and not only whitespace characters
+        const titleValid = BillTitle && BillTitle.split('').find(char => char !== ' ');
+        return titleValid && costValid;
+    };
+
+    const clearForm = () => {
+        setBillCost('');
+        setBillTitle('');
+    };
+    return (
+        <div className='add-bill-container'>
+            <input className='add-bill-form-control form-control'
+                placeholder='Enter Bill Title'
+                type='text'
+                value={BillTitle}
+                onChange={(e) => setBillTitle(e.target.value)}></input>
+            <input className='add-bill-form-control form-control'
+                placeholder='Enter Bill Monthly Cost'
+                type='number'
+                value={BillCost}
+                onChange={(e) => setBillCost(e.target.value)}></input>
+            <button className='add-bill-form-control btn btn-primary'
+                onClick={() => {
+                    if (billObjectValid()) {
+                        updateBills ();
+                        clearForm();
+                    }
+                }}> Add Bill</button>
+        </div>
+    );
+};
 export default AddBill;
